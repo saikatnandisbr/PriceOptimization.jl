54f1eeb7e83ab760a7267184ede33c2aa88c6575
Saikat Nandi <saikat.nandi@superiorbusinessresults.com>
2024-07-24 10:16:18 +0700

    Data and plotting update

--- src/scripts/run_price_optim.jl
+++ src/scripts/run_price_optim.jl
@@ -2,17 +2,27 @@
 
 # imports
 using .PriceOptimization
+using CSV
+using DataFrames
 using Plots
 
 # create optimizatio object
 optimObj = PriceOptimization.PriceOptim()         # call default constructor
 
 # prepare optimization model coefficients
-E = [-30.0, -20.0, -8.0]
-P = [11.1, 20.7, 10.1]
-C = [0.8, 0.7, 0.9] .* P
-Q = [100.0, 89.0, 60.0]
-ΔP_pct_max = [30.0, 30.0, 30.0]
+df = CSV.File("data/kaggle/supermarket_elasticities_cat.csv") |> DataFrame
+
+# E = [-30.0, -20.0, -8.0]
+# P = [11.1, 20.7, 10.1]
+# C = [0.8, 0.7, 0.9] .* P
+# Q = [100.0, 89.0, 60.0]
+# ΔP_pct_max = [30.0, 30.0, 30.0]
+
+E = 100 .* df.slope                                     # elasticity
+P = df.unit_price                                       # price
+C = round.(P .* rand(0.6:0.8, length(E)), digits=2)     # assign cost to be in range of price
+Q = Float64.(df.invoice_quantity)                       # quantity
+ΔP_pct_max = fill(5.0, length(E))                       # max allowed percent price change
 
 PriceOptimization.prep_optim_coefficients(optimObj, E=E, P=P, C=C, Q=Q, ΔP_pct_max=ΔP_pct_max)
 
@@ -31,7 +41,10 @@ optimObj.ΔP ./ P .* 100
 ΔR_pareto = Vector{Float64}()
 ΔGP_pareto = Vector{Float64}()
 
-for ΔGP in -30.0:5.0:30.0
+# choose a range for min GP change
+#   range chosen in feasible range
+for ΔGP in 160:10.0:260.0
+
     PriceOptimization.solve_optim(optimObj, ΔGP_min=ΔGP);
 
     push!(ΔR_pareto, optimObj.ΔR)
@@ -40,6 +53,6 @@ end
 
 plot(ΔR_pareto, ΔGP_pareto, legend=:none, xticks=round.(ΔR_pareto, digits=0), yticks=round.(ΔGP_pareto, digits=0))
 scatter!(ΔR_pareto, ΔGP_pareto, color=:blue, legend=:none)
-xlabel!("Change in Revenue")
-ylabel!("Change in Gross Profit")
-title!("Pareto Frontier for Price Optimization")
+xlabel!("Change in Revenue\n")
+ylabel!("\nChange in Gross Profit")
+title!("\nPareto Frontier for Price Optimization")
